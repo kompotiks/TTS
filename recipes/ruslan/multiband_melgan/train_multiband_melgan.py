56c197067f3fb8e9c17a958b98ba4652f9e29c31
kompotiks <lifami40@gmail.com>
2021-12-02 19:47:45 +0300

    New size batch 32 -> 64

--- recipes/ruslan/multiband_melgan/train_multiband_melgan.py
+++ recipes/ruslan/multiband_melgan/train_multiband_melgan.py
@@ -9,10 +9,10 @@ from TTS.vocoder.models.gan import GAN
 output_path = os.path.dirname(os.path.abspath(__file__))
 
 config = MultibandMelganConfig(
-    batch_size=32,
-    eval_batch_size=16,
-    num_loader_workers=4,
-    num_eval_loader_workers=4,
+    batch_size=64,
+    eval_batch_size=32,
+    num_loader_workers=8,
+    num_eval_loader_workers=8,
     run_eval=True,
     test_delay_epochs=5,
     epochs=1000,
